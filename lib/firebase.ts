1c6a14e3c88ea5eefa6659ba1cd89c09ea0e634d
Himanshu vaghela <himanshuvaghela@Himanshus-MacBook-Air.local>
2025-03-31 13:30:45 +0530

    changes signup

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,17 +2,14 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyABqn65A9aMmMETPIgn_je8OHfjdeHcmw4",
-  authDomain: "wildmind-212ae.firebaseapp.com",
-  projectId: "wildmind-212ae",
-  storageBucket: "wildmind-212ae.firebasestorage.app",
-  messagingSenderId: "399679382904",
-  appId: "1:399679382904:web:de8158abf4d5a698a4548d",
-  measurementId: "G-28DC9XNVEB"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
