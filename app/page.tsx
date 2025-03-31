871acf804a603ee387ae61cac3f03366164b0566
Himanshu vaghela <himanshuvaghela@Himanshus-MacBook-Air.local>
2025-03-31 15:54:22 +0530

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import Image from 'next/image';
-import { getImageUrl } from '../routes/imageroute';
 
 import "./globals.css";
 import Main from "./view/landingPage/page";
@@ -8,37 +6,9 @@ import Main from "./view/landingPage/page";
 // Define Page as a React functional component
 const Page: React.FC = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {/* Using core images */}
-      <div className="relative w-32 h-32">
-        <Image
-          src={getImageUrl('core', 'logo')}
-          alt="Logo"
-          fill
-          className="object-contain"
-        />
-      </div>
-
-      {/* Using auth images */}
-      <div className="relative w-full h-48">
-        <Image
-          src={getImageUrl('auth', 'loginBg')}
-          alt="Login Background"
-          fill
-          className="object-cover"
-        />
-      </div>
-
-      {/* Using dashboard images */}
-      <div className="relative w-16 h-16">
-        <Image
-          src={getImageUrl('dashboard', 'statsIcon')}
-          alt="Stats Icon"
-          fill
-          className="object-contain"
-        />
-      </div>
-    </main>
+    <>
+      <Main />
+    </>
   );
 };
 
