6daefb97f0e2ab26e390e05d677f60e245cd1a47
Aryan Prajapati <prajapatiaryan3103.com>
2025-04-22 15:34:27 +0530

    aryanprajapati

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,14 @@ import React from "react";
 
 import "./globals.css";
 // import Main from "./view/landingPage/page";
-// import ContactSection from "./view/contactus/page";
-import Main from "./view/landingPage/page";
+// import Main from "./view/landingPage/page";
+import ContactSection from "./view/contactus/page";
 
 // Define Page as a React functional component
 const Page: React.FC = () => {
   return (
     <>
-    
-      <Main />
+    <ContactSection />
     </>
   );
 };
