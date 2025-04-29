07250ca6812615b82c67c35909a7113e07d0e701
Aryan Prajapati <prajapatiaryan3103.com>
2025-04-29 17:41:27 +0530

    aryan

--- app/view/Blog/page.tsx
+++ app/view/Blog/page.tsx
@@ -1,24 +1,19 @@
-import CategorySections from "./components/Categorysections";
-import FeaturedArticles from "./components/FeaturedArticles";
-import FollowSection from "./components/Follow";
-import Header from "./components/header";
-
-
+import CategorySections from "./components/Categorysections"
+import FeaturedArticles from "./components/FeaturedArticles"
+import FollowSection from "./components/Follow"
+import Header from "./components/header"
 
 export default function Blog() {
   return (
-    <>
-    <div className="flex flex-col min-h-screen w-full bg-black text-white font-poppins">
+    <div className="flex flex-col min-h-screen w-full bg-black text-white font-poppins overflow-x-hidden">
       <Header />
 
       {/* Main Content */}
-      <main className="mt-20 pt-10 font-poppins">
-        <FeaturedArticles/>
+      <main className="mt-20 pt-10 font-poppins w-full">
+        <FeaturedArticles />
         <CategorySections />
         <FollowSection />
       </main>
     </div>
-    </>
   )
 }
-
