072120bf6bb7e512c57d050efc7050a3c6cb2bd7
Aryan Prajapati <prajapatiaryan3103.com>
2025-04-30 16:19:18 +0530

    messagearyanthegreat

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 // import Main from "./view/landingPage/page";
 // import Main from "./view/landingPage/page";
 // import ContactSection from "./view/contactus/page";
-import Home from "./view/home/[username]/page";
+// import Home from "./view/home/[username]/page";
 import Main from "./view/landingPage/page";
 
 // Define Page as a React functional component
