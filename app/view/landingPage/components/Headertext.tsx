bdb59adc7bc9da92152d7766eaa83a807c9cc432
Aryan Prajapati <prajapatiaryan3103.com>
2025-05-01 23:57:09 +0530

    messagearyanthegreat

--- app/view/landingPage/components/Headertext.tsx
+++ app/view/landingPage/components/Headertext.tsx
@@ -18,7 +18,7 @@ export function HeroHighlightDemo() {
           duration: 0.5,
            ease: [0.4, 0.0, 0.2, 1],
         }}
-        className="text-[1.3rem] px-4 md:text-4xl lg:text-5xl font-bold md:font-extrabold text-neutral-700 dark:text-white max-w-6xl leading-relaxed lg:leading-snug text-center  mt-14 mb:-mt-20 "
+        className="text-[1.5rem] px-4 md:text-4xl lg:text-5xl font-bold md:font-extrabold text-neutral-700 dark:text-white max-w-6xl leading-relaxed lg:leading-snug text-center  mt-14 mb:-mt-20 "
       >
         From your vision to Stunning Creations    {" "} <br/> 
         <Highlight className="text-black dark:text-white"> 
