94dc42b7ebb9c225d6d5c976a5065c60e30847d3
aryan3110A <prajapatiaryan3103@gmail.com>
2025-06-24 16:25:44 +0530

    message

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['firebasestorage.googleapis.com', 'placehold.co','api.wildmindai.com','api.wildmindai.com/medium'],
+    domains: ['firebasestorage.googleapis.com', 'placehold.co','api.wildmindai.com','api.wildmindai.medium/'],
   },
   
 }
