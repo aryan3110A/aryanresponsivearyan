72cf523a53b9034054698403a638bbd593c03ff7
rajdeep546 <rajdeepsinghchavda7@gmail.com>
2025-07-10 16:10:53 +0530

    product with model pose done _minor figma sync is in-complete

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,12 @@ import React from "react";
 // import Home from "./view/home/[username]/page";
 
 import "./globals.css";
+import ProductWithModelPosePage from "./view/BRANDINGKIT/PRODUCT_WITH_MODEL_POSE/page";
 // import Main from "./view/landingPage/page";
 // import Main from "./view/landingPage/page";
 // import ContactSection from "./view/contactus/page";
 // import Home from "./view/home/[username]/page";
-import Main from "./view/landingPage/page";
+// import Main from "./view/landingPage/page";
 // import ProductGeneration from "./view/BRANDINGKIT/PRODUCT_GENERATION/page";
 // import MockupGenerationPage from "./view/Mockupgeneration/page";
 // import NewText2Image from "./view/IMAGEGENERATIONNEW/newtexttoimage/page";
@@ -16,7 +17,9 @@ import Main from "./view/landingPage/page";
 const Page: React.FC = () => {
   return (
     <>
-    <Main />
+    {/* <Main /> */}
+    <ProductWithModelPosePage/>
+    
     {/* <ProductGeneration /> */}
     {/* <AISTICKERGEN /> */}
 {/* <NewText2Image /> */}
