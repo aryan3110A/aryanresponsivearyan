72b727e3c7b816acc47d6467fcf40ca8dc6710d5
Himanshusinh <itzhimanshu3107@gmail.com>
2025-07-31 17:39:29 +0530

    main

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import TextToMusic from './view/musicgeneration/texttomusic/page'
+import Main from './view/landingPage/page';
 
 // Define Page as a React functional component
 const Page: React.FC = () => {
   return (
     <>
-      {/* <Main /> */}
+      <Main />
       {/* <InpaintFluxAPI/> */}
       {/* <ImageLibrary/> */}
       {/* <InChatHistory/> */}
-      <TextToMusic />
+      {/* <TextToMusic /> */}
       
     </>
   );
